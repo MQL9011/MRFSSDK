e3dd4c8f6e659b057e0c66bde27a9c43c6785d73
mccree <301063915@qq.com>
2021-03-10 14:50:37 +0800

    基座打包配置

---
+++ UniPlugin-MRFSSDK/ios/MRFSSDK.framework/Headers/MRFSSDK.h
@@ -0,0 +1,29 @@
+//
+//  MRFSSDK.h
+//  MRFSSDK
+//
+//  Created by mccree on 2021/3/9.
+//
+
+#import <Foundation/Foundation.h>
+#import "DCUni/DCUniModule.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MRFSSDK : DCUniModule
+
++ (instancetype)sharedInstance;
+
+/// 初始化
+- (void)configFSMeeting;
+
+/// 加入会议室
+/// @param param 参数
+- (void)joinFSMeeting:(NSDictionary *)param;
+
+/// 获取当前控制器
++ (UIViewController *)rootViewController;
+
+@end
+
+NS_ASSUME_NONNULL_END
